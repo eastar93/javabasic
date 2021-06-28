c663b9181e206037694e41aa3f52c2fd267fcb7e
eastar93 <eastar93@naver.com>
2021-06-28 17:44:57 +0900

    eastar93

---
+++ 11-javastatic/src/Static01/Main01.java
@@ -0,0 +1,11 @@
+package Static01;
+
+public class Main01 {
+	
+	public static void main(String[] args) {
+		StaticTest s1 = new StaticTest();
+		StaticTest s2 = new StaticTest();
+		StaticTest s3 = new StaticTest();
+		
+	}
+}
