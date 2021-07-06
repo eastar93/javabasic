c6bbb2010abb6093f3a18b375b15ba0cf40a9151
eastar93 <eastar93@naver.com>
2021-07-06 13:27:16 +0900

    eastar93

---
+++ 16-javaapi/src/String01/String06.java
@@ -0,0 +1,19 @@
+package String01;
+
+public class String06 {
+	
+	public static void main(String[] args) {
+		//substring()은 인덱스 기준으로 앞 문자열을 삭제합니다.
+		//인덱스 번호를 두 개 주면 처음 인덱스 번호 ~ 두번째 인덱스 번호 사이만 남겨줍니다.
+		
+		String str = "자바자바JSPJSP스프링스프링";
+		String result = str.substring(5);
+		//5번 자리를 시작점으로 잡고 나머지 0번자리부터 4번자리에 있는 문자를 삭제한다.
+		System.out.println(result);
+		
+		result = str.substring(10, 15);
+		//10번부터 15번사이(10~14)에 있는 문자열만 쓰고 나머지 배치되어있는 문자를 삭제한다.
+		System.out.println(result);
+	}
+
+}
---
+++ 16-javaapi/src/StringBuilder01/Builder05.java
@@ -0,0 +1,20 @@
+package StringBuilder01;
+
+public class Builder05 {
+	
+	public static void main(String[] args) {
+		//다음 문자열의 문자를 3번째마다 삭제해주세요.
+		//힌트 : 반복문을 쓰세요
+		
+		StringBuilder a = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+		
+		for(int i = 0; i < a.length(); i++)	{
+			if(i%3==0) {
+				a.deleteCharAt(i);	
+			}
+		}
+		System.out.println(a);
+		
+	}
+
+}
