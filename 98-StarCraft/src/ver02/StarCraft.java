4b3565319fed00c29e98ed155f0791ff73e79fb0
eastar93 <eastar93@naver.com>
2021-08-03 16:55:18 +0900

    eastar93

---
+++ 98-StarCraft/src/ver02/StarCraft.java
@@ -0,0 +1,17 @@
+package ver02;
+
+public class StarCraft {
+	
+	public static void main(String[] args) {
+		
+		Marine m1 = new Marine();
+		m1.getInfo();
+		m1.GetHurtUltra();
+		m1.Cure();
+		m1.GetHurtHydra();
+		m1.Cure();
+		
+		 
+		
+	}
+}
